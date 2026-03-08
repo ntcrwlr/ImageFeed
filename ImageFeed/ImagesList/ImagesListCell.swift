6054b4524f4bb61d9fb14b6137d4ddf7a38c0b8d
Serg <bushkov.s.p@icloud.com>
2026-03-08 12:04:19 +0300

    fixed the application based on the reviewer's comment

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,8 +5,8 @@
 //  Created by Сергей Бушков on 02.03.2026.
 //
 
-import Foundation
 import UIKit
+import QuartzCore
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
@@ -14,4 +14,22 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var cellImage: UIImageView!
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var dateLabel: UILabel!
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addGradientToImage()
+    }
+    
+    private func addGradientToImage() {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor(white: 0, alpha: 0.6).cgColor]
+        gradientLayer.locations = [0.6, 1.0]
+        cellImage.layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = cellImage.bounds
+    }
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         tableView.delegate = self
         tableView.dataSource = self
     }
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
+    private let today = Date()
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -46,17 +46,17 @@ extension ImagesListViewController: UITableViewDelegate {
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        photosName.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-                
-                guard let imageListCell = cell as? ImagesListCell else {
-                    return UITableViewCell()
-                }
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
         configCell(for: imageListCell, with: indexPath)
-                return imageListCell
+        return imageListCell
     }
 
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
@@ -67,8 +67,9 @@ extension ImagesListViewController: UITableViewDataSource {
         cell.cellImage.image = image
         cell.dateLabel.text = dateFormatter.string(from: Date())
 
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        let isLiked: Bool = indexPath.row % 2 == 0
+        let imageResource: ImageResource = isLiked ? .likeButtonOn : .likeButtonOff
+        let likeImage: UIImage = UIImage(resource: imageResource)
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
